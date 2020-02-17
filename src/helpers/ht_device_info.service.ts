0282087ec2a076fb3bd663c6f7c62d3bc9a42d0f
Justin Crosbie <justin.crosbie@haventec.com>
2020-02-17 16:37:01 +1100

    Made more robust

--- src/helpers/ht_device_info.service.ts
+++ src/helpers/ht_device_info.service.ts
@@ -29,7 +29,6 @@ class DeviceInfoService {
         try {
             Fingerprint2.get((components: Fingerprint2.Component[]) => {
                 this.fingerprintComponents = components;
-                this.getFingerprintComponents();
             });
         } catch (error) {
             throw new HT_Error(ErrorCode.HT_CM_EXTERNAL, ErrorMessage.FINGERPRINT_CREATION_ERROR);
@@ -122,116 +121,117 @@ class DeviceInfoService {
 
         if ( !this.fingerprintDetails ) {
             this.fingerprintDetails = new Array();
-        }
 
-        if ( this.fingerprintComponents ) {
-            for ( let i = 0; i < this.fingerprintComponents.length; i++ ) {
-                if ( this.fingerprintComponents[i] &&
-                    this.fingerprintComponents[i].key !== 'canvas' &&
-                    this.fingerprintComponents[i].key !== 'webgl' ) {
+            if ( this.fingerprintComponents ) {
+                for ( let i = 0; i < this.fingerprintComponents.length; i++ ) {
+                    if ( this.fingerprintComponents[i] &&
+                        this.fingerprintComponents[i].key !== 'canvas' &&
+                        this.fingerprintComponents[i].key !== 'webgl' ) {
 
-                    if ( this.fingerprintComponents[i].key === 'js_fonts' ) {
-                        this.fonts = this.fingerprintComponents[i].key;
+                        if ( this.fingerprintComponents[i].key === 'js_fonts' ) {
+                            this.fonts = this.fingerprintComponents[i].key;
+                        }
+                        this.fingerprintDetails.push({
+                            key: this.fingerprintComponents[i].key,
+                            value: this.joinValues(this.fingerprintComponents[i].value)
+                        });
                     }
-                    this.fingerprintDetails.push({
-                        key: this.fingerprintComponents[i].key,
-                        value: this.joinValues(this.fingerprintComponents[i].value)
-                    });
                 }
+
+
+                // FingerprintJS does not generate these
+                this.pushFPC(this.fingerprintDetails, 'osType', this.getOsType());
+                this.pushFPC(this.fingerprintDetails, 'browserType', this.getBrowser());
+                this.pushFPC(this.fingerprintDetails, 'browserVersion', this.getBrowserVersion());
+                this.pushFPC(this.fingerprintDetails, 'deviceName', this.getDeviceName());
+                this.pushFPC(this.fingerprintDetails, 'userAgent', navigator.userAgent);
+
+                this.getFingerprint();
+
+                if ( this.getBrowserVersion() ) {
+                    this.pushFPC(this.fingerprintDetails, 'browser_major_version', this.getBrowserMajorVersion());
+                }
+
+                if ( this.browserData ) {
+                    if ( this.browserData.engine ) {
+                        this.pushFPC(this.fingerprintDetails, 'engine', this.browserData.engine.name);
+                        this.pushFPC(this.fingerprintDetails, 'engine_version', this.browserData.engine.version);
+                    }
+                    if ( this.browserData.os ) {
+                        this.pushFPC(this.fingerprintDetails, 'engine_version', this.browserData.os.version);
+                    }
+                    if ( this.browserData.device ) {
+                        this.pushFPC(this.fingerprintDetails, 'device_type', this.browserData.device.type);
+                        this.pushFPC(this.fingerprintDetails, 'device_vendor', this.browserData.device.vendor);
+                    }
+                    if ( this.browserData.cpu ) {
+                        this.pushFPC(this.fingerprintDetails, 'cpu', this.browserData.architecture);
+                    }
+                }
+
+                if ( screen ) {
+                    this.pushFPC(this.fingerprintDetails, 'device_xdpi', screen['deviceXDPI']);
+                    this.pushFPC(this.fingerprintDetails, 'device_ydpi', screen['deviceYDPI']);
+                }
+
+                this.pushFPC(this.fingerprintDetails, 'timezone', this.getTimeZone());
+
+                this.pushFPC(this.fingerprintDetails, 'is_mime_types', this.isMimeTypes());
+                this.pushFPC(this.fingerprintDetails, 'mime_types', this.getMimeTypes());
+                this.pushFPC(this.fingerprintDetails, 'system_language', this.getSystemLanguage());
+
+                /**
+                 * This is the list of items from ClientJS that we cover
+                 */
+                // this.pushFPC(fc, 'fingerprint', this.clientjs.getFingerprint());
+                // this.pushFPC(fc, 'user_agent', this.clientjs.getUserAgent());
+                // this.pushFPC(fc, 'browserType', this.clientjs.getBrowser());
+                // this.pushFPC(fc, 'browserVersion', this.clientjs.getBrowserVersion());
+                // this.pushFPC(fc, 'browser_major_version', this.clientjs.getBrowserMajorVersion());
+                // this.pushFPC(fc, 'engine', this.clientjs.getEngine());
+                // this.pushFPC(fc, 'engine_version', this.clientjs.getEngineVersion());
+                // this.pushFPC(fc, 'osType', this.clientjs.getOS());
+                // this.pushFPC(fc, 'os_version', this.clientjs.getOSVersion());
+                // this.pushFPC(fc, 'device', this.clientjs.getDevice());
+                // this.pushFPC(fc, 'device_type', this.clientjs.getDeviceType());
+                // this.pushFPC(fc, 'device_vendor', this.clientjs.getDeviceVendor());
+                // this.pushFPC(fc, 'cpu', this.clientjs.getCPU());
+                // this.pushFPC(fc, 'is_mobile', this.clientjs.isMobile());
+                // this.pushFPC(fc, 'is_mobile_major', this.clientjs.isMobileMajor());
+                // this.pushFPC(fc, 'is_mobile_android', this.clientjs.isMobileAndroid());
+                // this.pushFPC(fc, 'is_mobile_opera', this.clientjs.isMobileOpera());
+                // this.pushFPC(fc, 'is_mobile_windows', this.clientjs.isMobileWindows());
+                // this.pushFPC(fc, 'is_mobile_blackberry', this.clientjs.isMobileBlackBerry());
+                // this.pushFPC(fc, 'is_mobile_ios', this.clientjs.isMobileIOS());
+                // this.pushFPC(fc, 'is_iphone', this.clientjs.isIphone());
+                // this.pushFPC(fc, 'is_ipad', this.clientjs.isIpad());
+                // this.pushFPC(fc, 'is_ipod', this.clientjs.isIpod());
+                // this.pushFPC(fc, 'screen_print', this.clientjs.getScreenPrint());
+                // this.pushFPC(fc, 'resolution', this.clientjs.getCurrentResolution());
+                // this.pushFPC(fc, 'available_resolution', this.clientjs.getAvailableResolution());
+                //
+                // this.pushFPC(fc, 'device_xdpi', this.clientjs.getDeviceXDPI());
+                // this.pushFPC(fc, 'device_ydpi', this.clientjs.getDeviceYDPI());
+                // this.pushFPC(fc, 'plugins', this.clientjs.getPlugins());
+                // this.pushFPC(fc, 'timezone', this.clientjs.getTimeZone());
+                // this.pushFPC(fc, 'is_mime_types', this.clientjs.isMimeTypes());
+                // this.pushFPC(fc, 'mime_types', this.clientjs.getMimeTypes());
+                // this.pushFPC(fc, 'is_local_storage', this.clientjs.isLocalStorage());
+                // this.pushFPC(fc, 'language', this.clientjs.getLanguage());
+                // this.pushFPC(fc, 'system_language', this.clientjs.getSystemLanguage());
+
+                /**
+                 * This is the list of items from ClientJS that we DO NOT cover
+                 */
+                // this.pushFPC(fc, 'is_java', this.clientjs.isJava());
+                // this.pushFPC(fc, 'java_version', this.clientjs.getJavaVersion());
+                // this.pushFPC(fc, 'is_flash', this.clientjs.isFlash());
+                // this.pushFPC(fc, 'flash_version', this.clientjs.getFlashVersion());
+                // this.pushFPC(fc, 'is_silverlight', this.clientjs.isSilverlight());
+                // this.pushFPC(fc, 'silverlight_version', this.clientjs.getSilverlightVersion());
+                // this.pushFPC(fc, 'is_canvas', this.clientjs.isCanvas());
             }
         }
-
-        // FingerprintJS does not generate these
-        this.pushFPC(this.fingerprintDetails, 'osType', this.getOsType());
-        this.pushFPC(this.fingerprintDetails, 'browserType', this.getBrowser());
-        this.pushFPC(this.fingerprintDetails, 'browserVersion', this.getBrowserVersion());
-        this.pushFPC(this.fingerprintDetails, 'deviceName', this.getDeviceName());
-        this.pushFPC(this.fingerprintDetails, 'userAgent', navigator.userAgent);
-
-        this.getFingerprint();
-
-        if ( this.getBrowserVersion() ) {
-            this.pushFPC(this.fingerprintDetails, 'browser_major_version', this.getBrowserMajorVersion());
-        }
-
-        if ( this.browserData ) {
-            if ( this.browserData.engine ) {
-                this.pushFPC(this.fingerprintDetails, 'engine', this.browserData.engine.name);
-                this.pushFPC(this.fingerprintDetails, 'engine_version', this.browserData.engine.version);
-            }
-            if ( this.browserData.os ) {
-                this.pushFPC(this.fingerprintDetails, 'engine_version', this.browserData.os.version);
-            }
-            if ( this.browserData.device ) {
-                this.pushFPC(this.fingerprintDetails, 'device_type', this.browserData.device.type);
-                this.pushFPC(this.fingerprintDetails, 'device_vendor', this.browserData.device.vendor);
-            }
-            if ( this.browserData.cpu ) {
-                this.pushFPC(this.fingerprintDetails, 'cpu', this.browserData.architecture);
-            }
-        }
-
-        if ( screen ) {
-            this.pushFPC(this.fingerprintDetails, 'device_xdpi', screen['deviceXDPI']);
-            this.pushFPC(this.fingerprintDetails, 'device_ydpi', screen['deviceYDPI']);
-        }
-
-        this.pushFPC(this.fingerprintDetails, 'timezone', this.getTimeZone());
-
-        this.pushFPC(this.fingerprintDetails, 'is_mime_types', this.isMimeTypes());
-        this.pushFPC(this.fingerprintDetails, 'mime_types', this.getMimeTypes());
-        this.pushFPC(this.fingerprintDetails, 'system_language', this.getSystemLanguage());
-
-        /**
-         * This is the list of items from ClientJS that we cover
-         */
-        // this.pushFPC(fc, 'fingerprint', this.clientjs.getFingerprint());
-        // this.pushFPC(fc, 'user_agent', this.clientjs.getUserAgent());
-        // this.pushFPC(fc, 'browserType', this.clientjs.getBrowser());
-        // this.pushFPC(fc, 'browserVersion', this.clientjs.getBrowserVersion());
-        // this.pushFPC(fc, 'browser_major_version', this.clientjs.getBrowserMajorVersion());
-        // this.pushFPC(fc, 'engine', this.clientjs.getEngine());
-        // this.pushFPC(fc, 'engine_version', this.clientjs.getEngineVersion());
-        // this.pushFPC(fc, 'osType', this.clientjs.getOS());
-        // this.pushFPC(fc, 'os_version', this.clientjs.getOSVersion());
-        // this.pushFPC(fc, 'device', this.clientjs.getDevice());
-        // this.pushFPC(fc, 'device_type', this.clientjs.getDeviceType());
-        // this.pushFPC(fc, 'device_vendor', this.clientjs.getDeviceVendor());
-        // this.pushFPC(fc, 'cpu', this.clientjs.getCPU());
-        // this.pushFPC(fc, 'is_mobile', this.clientjs.isMobile());
-        // this.pushFPC(fc, 'is_mobile_major', this.clientjs.isMobileMajor());
-        // this.pushFPC(fc, 'is_mobile_android', this.clientjs.isMobileAndroid());
-        // this.pushFPC(fc, 'is_mobile_opera', this.clientjs.isMobileOpera());
-        // this.pushFPC(fc, 'is_mobile_windows', this.clientjs.isMobileWindows());
-        // this.pushFPC(fc, 'is_mobile_blackberry', this.clientjs.isMobileBlackBerry());
-        // this.pushFPC(fc, 'is_mobile_ios', this.clientjs.isMobileIOS());
-        // this.pushFPC(fc, 'is_iphone', this.clientjs.isIphone());
-        // this.pushFPC(fc, 'is_ipad', this.clientjs.isIpad());
-        // this.pushFPC(fc, 'is_ipod', this.clientjs.isIpod());
-        // this.pushFPC(fc, 'screen_print', this.clientjs.getScreenPrint());
-        // this.pushFPC(fc, 'resolution', this.clientjs.getCurrentResolution());
-        // this.pushFPC(fc, 'available_resolution', this.clientjs.getAvailableResolution());
-        //
-        // this.pushFPC(fc, 'device_xdpi', this.clientjs.getDeviceXDPI());
-        // this.pushFPC(fc, 'device_ydpi', this.clientjs.getDeviceYDPI());
-        // this.pushFPC(fc, 'plugins', this.clientjs.getPlugins());
-        // this.pushFPC(fc, 'timezone', this.clientjs.getTimeZone());
-        // this.pushFPC(fc, 'is_mime_types', this.clientjs.isMimeTypes());
-        // this.pushFPC(fc, 'mime_types', this.clientjs.getMimeTypes());
-        // this.pushFPC(fc, 'is_local_storage', this.clientjs.isLocalStorage());
-        // this.pushFPC(fc, 'language', this.clientjs.getLanguage());
-        // this.pushFPC(fc, 'system_language', this.clientjs.getSystemLanguage());
-
-        /**
-         * This is the list of items from ClientJS that we DO NOT cover
-         */
-        // this.pushFPC(fc, 'is_java', this.clientjs.isJava());
-        // this.pushFPC(fc, 'java_version', this.clientjs.getJavaVersion());
-        // this.pushFPC(fc, 'is_flash', this.clientjs.isFlash());
-        // this.pushFPC(fc, 'flash_version', this.clientjs.getFlashVersion());
-        // this.pushFPC(fc, 'is_silverlight', this.clientjs.isSilverlight());
-        // this.pushFPC(fc, 'silverlight_version', this.clientjs.getSilverlightVersion());
-        // this.pushFPC(fc, 'is_canvas', this.clientjs.isCanvas());
 
         return this.fingerprintDetails;
     }
